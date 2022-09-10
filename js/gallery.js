5a5be579f6e0ddfe32faa861f034259ef0dd892b
Ahmad jz <ahmadjzz99@gmail.com>
2022-09-10 20:48:36 +0300

    solution

--- js/gallery.js
+++ js/gallery.js
@@ -1,22 +1,11 @@
-/*Name this external file gallery.js*/
-
 function upDate(previewPic) {
   mydiv = document.getElementById("image");
-  /* In this function you should 
-       1) change the url for the background image of the div with the id = "image" 
-       to the source file of the preview image
-       
-       2) Change the text  of the div with the id = "image" 
-       to the alt text of the preview image 
-       */
+  mydiv.style.backgroundImage = "url(" + previewPic.src + ")";
+  mydiv.innerHTML = previewPic.alt;
 }
 
 function unDo() {
-  /* In this function you should 
-       1) Update the url for the background image of the div with the id = "image" 
-       back to the orginal-image.  You can use the css code to see what that original URL was
-       
-       2) Change the text  of the div with the id = "image" 
-       back to the original text.  You can use the html code to see what that original text was
-       */
+  mydiv = document.getElementById("image");
+  mydiv.style.backgroundImage = "url()";
+  mydiv.innerHTML = "Hover over an image below to display here.";
 }
